ca479fd23a599b02362752635d6e92f719761859
MasumHossain71 <masumhazi311@gmail.com>
2023-02-01 22:46:48 +0600

    load data using fetch

--- fetch.js
+++ fetch.js
@@ -1,1 +1,17 @@
-const loadPosts = fetch('https://jsonplaceholder.typicode.com/posts')
+const loadPosts ='https://jsonplaceholder.typicode.com/posts'
+fetch(loadPosts).then(res => res.json()).then(data => displayPosts(data))
+
+
+function displayPosts(posts){
+   const postContainer = document.getElementById('post-container');
+   for(const post of posts){
+      const div = document.createElement('div');
+      div.classList.add('posts')
+      div.innerHTML =`
+      <h4>User- ${post.userId}</h4>
+      <h5>Post-title: ${post.title}</h5>
+      <p>${post.body}</p>
+      `
+      postContainer.appendChild(div);
+   }
+}
